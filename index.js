b0e5fe600e663209da56c7b6757d76262441dded
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2021-04-06 18:50:17 +0000

    submitting svgForm

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var express = require('express');
 var bodyParser = require("body-parser");
 //require node-fetch
 var fetch = require('node-fetch');
+// require mongodb
+const MongoClient = require('mongodb').MongoClient;
 //create express object, call express
 var app = express();
 
@@ -19,6 +21,18 @@ app.use(express.static("public"));
 //tell app to use Body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+
+//const uri = "mongodb+srv://Gardener:GardenerPassword@cluster2.rkbio.mongodb.net/plots?retryWrites=true&w=majority";
+//const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//client.connect(err => {
+  //const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  //client.close();
+//});
+
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -27,6 +41,43 @@ app.use(bodyParser.urlencoded({extended: true}));
 //home page
 app.get('/',function(req,res){
     res.render('index');
+});
+
+app.post('/cropForm',(req,res)=>{
+    //intall the SMTP server
+    console.log("form sent");
+    const smtpTrans = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'projectgarden706@gmail.com', 
+            pass: 'GardenProject5!'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+    var user = req.body.personEmail;
+    var plot = req.body.plotNumber;
+    var crop= req.body.chosenCrop;
+    var date= req.body.datetimepicker1;
+    //specify what the email will look like
+    const mailOpts = {
+        from: user,
+        to: 'projectgarden706@gmail.com',
+        subject: 'You have a new message from Community Garden Website',
+        text: user + ' reserved '+ plot + ' and will be planting'+ crop + ' on '+ date + '.'
+    }
+
+    smtpTrans.sendMail(mailOpts, function (err, res) {
+        if (err) {
+            console.error('there was an error: ', err);
+        }
+        else {
+            console.log("Message was sent!");
+            sent = true;
+        }
+    })
+    res.redirect('/');
 });
 
 //volunteer page
@@ -41,39 +92,39 @@ app.get('/contact', function(req, res){
     res.render('contact',  { sent: sent });
 });
 
-app.post('/sendEmail', (req, res) => {
+app.post('/sendEmail', (req, res) => {});
     //intall the SMTP server
-    const smtpTrans = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'projectgarden706@gmail.com', 
-            pass: 'GardenProject5!'
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
-    var user = req.body.email;
-    var message = req.body.message;
+    //const smtpTrans = nodemailer.createTransport({
+      //  service: 'Gmail',
+        //auth: {
+          //  user: 'projectgarden706@gmail.com', 
+            //pass: 'GardenProject5!'
+        //},
+        //tls: {
+          //  rejectUnauthorized: false
+        //}
+    //})
+    //var user = req.body.email;
+    //var message = req.body.message;
     //specify what the email will look like
-    const mailOpts = {
-        from: user,
-        to: 'projectgarden706@gmail.com',
-        subject: 'You have a new message from Community Garden Website',
-        text: user + ' wrote: ' + message
-    }
+    //const mailOpts = {
+      //  from: user,
+        //to: 'projectgarden706@gmail.com',
+        //subject: 'You have a new message from Community Garden Website',
+        //text: user + ' wrote: ' + message
+    //}
 
-    smtpTrans.sendMail(mailOpts, function (err, res) {
-        if (err) {
-            console.error('there was an error: ', err);
-        }
-        else {
-            console.log("Message was sent!");
-            sent = true;
-        }
-    })
-    res.redirect('/contact');
-});
+    //smtpTrans.sendMail(mailOpts, function (err, res) {
+      //  if (err) {
+        //    console.error('there was an error: ', err);
+        //}
+        //else {
+          //  console.log("Message was sent!");
+            //sent = true;
+        //}
+    //})
+    //res.redirect('/contact');
+//});
 
 
 //<<<<<<< AshleyBranch
